c07494f3692fd291a2881d1a2ba07f1c5ac3a22b
Edward <ekreutze@uvm.edu>
2018-03-25 13:10:19 -0400

    Changed QR code source

--- src/jumbo.vue
+++ src/jumbo.vue
@@ -18,23 +18,26 @@
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn flat @click="goToWebsite">Full Website</v-btn>
       <v-btn flat @click.stop="showDownloads = true">Download App</v-btn>
-      <v-dialog v-model="showDownloads" max-width="auto">
+      <v-dialog v-model="showDownloads" max-width="700px">
           <v-card>
             <v-card-toolbar>
               <h4 style="font-weight: 100; font-size: 25px">Download the app</h4>
             </v-card-toolbar>
             <v-card-text class="mx-auto">
               <v-layout row>
-                  <v-card class="mr-5 ml-2">
+                  <v-card class="mr-5 ml-2" color="white">
+                      
                   <v-flex xs6 class="">
-                      <img src="./Unitag_QRCode_40175936.png">
+                      <a href="http://www.unitag.io/qreator/generate?setting=%7B%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22transparent%22%2C%22COLOR1%22%3A%2268b7e7%22%7D%2C%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22ER_IC%22%7D%2C%22BODY_TYPE%22%3A5%2C%22E%22%3A%22H%22%2C%22ARRONDI%22%3A10%2C%22LOGO%22%3A%7B%22L_NAME%22%3A%22https%3A%2F%2Fstatic-unitag.com%2Ffile%2Ffreeqr%2F278a02a2fde68b906ea8676311e01693.png%22%2C%22EXCAVATE%22%3Atrue%7D%7D&data=%7B%22TYPE%22%3A%22url%22%2C%22DATA%22%3A%7B%22URL%22%3A%22https%3A%2F%2Fitunes.apple.com%2Fus%2Fapp%2Frhume%2Fid1341372546%3Fmt%3D8%22%7D%7D "><img src="http://www.unitag.io/qreator/generate?setting=%7B%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22transparent%22%2C%22COLOR1%22%3A%2268b7e7%22%7D%2C%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22ER_IC%22%7D%2C%22BODY_TYPE%22%3A5%2C%22E%22%3A%22H%22%2C%22ARRONDI%22%3A10%2C%22LOGO%22%3A%7B%22L_NAME%22%3A%22https%3A%2F%2Fstatic-unitag.com%2Ffile%2Ffreeqr%2F278a02a2fde68b906ea8676311e01693.png%22%2C%22EXCAVATE%22%3Atrue%7D%7D&data=%7B%22TYPE%22%3A%22url%22%2C%22DATA%22%3A%7B%22URL%22%3A%22https%3A%2F%2Fitunes.apple.com%2Fus%2Fapp%2Frhume%2Fid1341372546%3Fmt%3D8%22%7D%7D" alt="QR Code - "></a>
                   </v-flex>
+                  <v-card-title class="mx-auto" style="font-weight: 100; font-size: 18px">iOS App Store</v-card-title>
                   </v-card>
                
                   <v-card>
                   <v-flex xs6>
-                      <img src="./Unitag_QRCode_Rhume-gPlay.png">
+                      <a href="http://www.unitag.io/qreator/generate?setting=%7B%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22ER_IR%22%7D%2C%22E%22%3A%22H%22%2C%22LOGO%22%3A%7B%22EXCAVATE%22%3Atrue%2C%22L_Y%22%3A0%2C%22L_X%22%3A0%2C%22L_X_Norm%22%3Anull%2C%22L_Y_Norm%22%3Anull%2C%22L_WIDTH%22%3A0%2C%22L_LENGTH%22%3A0%2C%22L_NAME%22%3A%22https%3A%2F%2Fstatic-unitag.com%2Ffile%2Fqr%2F2ea311882012dea08be8120b981d969c.png%22%7D%2C%22BODY_TYPE%22%3A5%2C%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22transparent%22%2C%22COLOR1%22%3A%2268b7e7%22%7D%7D&data=%7B%22DATA%22%3A%7B%22URL%22%3A%22http%3A%5C%2F%5C%2Fopn.to%5C%2Fa%5C%2FZPk4f%22%7D%2C%22TYPE%22%3A%22url%22%7D "><img src="http://www.unitag.io/qreator/generate?setting=%7B%22EYES%22%3A%7B%22EYE_TYPE%22%3A%22ER_IR%22%7D%2C%22E%22%3A%22H%22%2C%22LOGO%22%3A%7B%22EXCAVATE%22%3Atrue%2C%22L_Y%22%3A0%2C%22L_X%22%3A0%2C%22L_X_Norm%22%3Anull%2C%22L_Y_Norm%22%3Anull%2C%22L_WIDTH%22%3A0%2C%22L_LENGTH%22%3A0%2C%22L_NAME%22%3A%22https%3A%2F%2Fstatic-unitag.com%2Ffile%2Fqr%2F2ea311882012dea08be8120b981d969c.png%22%7D%2C%22BODY_TYPE%22%3A5%2C%22LAYOUT%22%3A%7B%22COLORBG%22%3A%22transparent%22%2C%22COLOR1%22%3A%2268b7e7%22%7D%7D&data=%7B%22DATA%22%3A%7B%22URL%22%3A%22http%3A%5C%2F%5C%2Fopn.to%5C%2Fa%5C%2FZPk4f%22%7D%2C%22TYPE%22%3A%22url%22%7D" alt="QR Code - Rhume-gPlay"></a>
                   </v-flex>
+                  <v-card-title class="mx-auto" style="font-weight: 100; font-size: 18px">Google Play Store</v-card-title>
                   </v-card>
               </v-layout>
             </v-card-text>
